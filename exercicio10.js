67f1df8770336fea5612a8d078e89c683366c4ee
Raiane Yanagita - Turma 32 - Tribo A <raianemayumeyanagita@gmail.com>
2023-03-29 20:19:57 -0300

    exercicio

--- exercicio10.js
+++ exercicio10.js
@@ -1,6 +1,6 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
-let palavraMaior = 0;
-let palavraMenor = 0;
+let palavraMaior = '';
+let palavraMenor = '';
 
 
 for (let index = 0; index < array.length; index += 1) {
