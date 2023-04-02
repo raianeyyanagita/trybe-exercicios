7b49093815aac49851c53095cea58e79dc5f5187
Raiane Yanagita - Turma 32 - Tribo A <raianemayumeyanagita@gmail.com>
2023-04-02 20:56:58 -0300

    Exercicio objeto

---
+++ fundamentos-secao-3-introducao-a-javascript/exercicio1.js
@@ -0,0 +1,25 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+info['recorrente'] = 'Sim';
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+//for(let key in info) {
+//console.log(Object.values(info, info2));
+//} console.log('Bem-vinda, ' + info.personagem);
+//function concatenar(info) {
+ //   return Object.values(info) + Object.values(info2);
+//}
+//console.log(concatenar(info));
+
+
+
+
